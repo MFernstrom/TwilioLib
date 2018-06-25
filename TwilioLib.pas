bfae97191bc109ec43917956a205a7d4c3d73129
Marcus Fernstrom <MFernstrom@users.noreply.github.com>
2018-06-24 21:23:36 -0400

    Wrapped in try/finally, added assign() to not create reference

--- TwilioLib.pas
+++ TwilioLib.pas
@@ -1,7 +1,7 @@
 {======================================================
   Author      Marcus Fernström
   License     GPL3
-  Version     0.1
+  Version     0.2
   GitHub      https://github.com/MFernstrom/TwilioLib
 =======================================================}
 unit TwilioLib;
@@ -34,52 +34,59 @@ begin
   url := 'https://api.twilio.com/2010-04-01/Accounts/' + sid + '/Messages.json';
 end;
 
-procedure TTwilio.send_sms( from_number, to_number, message: String; VAR Output:TStringList );
+procedure TTwilio.send_sms( from_number, to_number, message: String; Var Output:TStringList );
 var
   resultVar, postVars: TStringlist;
-  s: String;
-  Respo: TStringStream;
+  responseData: TStringStream;
   pos: Integer;
   jData : TJSONData;
   jObject : TJSONObject;
 begin
-  if Not Assigned(Output) then
-    raise exception.create('Output variable isn''t initialized') at
-    get_caller_addr(get_frame),
-    get_caller_frame(get_frame);
-
-  // Initialise result variable, and set defaults
   resultVar := TStringlist.Create;
-  resultVar.CommaText := 'sid=,date_created=,date_updated=,date_sent=,account_sid=,to=,from=,body=,status=,num_segments=,num_media=,direction=,api_version=,price=,price_unit=,error_code=,error_message=,uri=,raw=';
   postVars := TStringlist.Create;
-  postVars.Add('To=' + to_number);
-  postVars.Add('From=' + from_number);
-  postVars.Add('Body=' + message);
+  responseData := TStringStream.Create('');
+  try
+    // Check if the output variable has been initialized
+    if Not Assigned(Output) then
+      raise exception.create('Output variable isn''t initialized') at
+      get_caller_addr(get_frame),
+      get_caller_frame(get_frame);
 
-  With TFPHttpClient.Create(Nil) do
-    try
-      Respo:=TStringStream.Create('');
-      UserName := account_sid;
-      Password := auth_token;
-      FormPost(url, postVars, Respo);
-      S := Respo.DataString;
-      Respo.Destroy;
-    finally
-      Free;
+    // Prep resultVar
+    resultVar.CommaText := 'sid=,date_created=,date_updated=,date_sent=,account_sid=,to=,from=,body=,status=,num_segments=,num_media=,direction=,api_version=,price=,price_unit=,error_code=,error_message=,uri=,raw=';
+
+    // postVars holds the API data needed, to/from/body
+    postVars.Add('To=' + to_number);
+    postVars.Add('From=' + from_number);
+    postVars.Add('Body=' + message);
+
+    // Make the api call
+    With TFPHttpClient.Create(Nil) do
+      try
+        UserName := account_sid;
+        Password := auth_token;
+        FormPost(url, postVars, responseData);
+      finally
+        Free;
+      end;
+
+    jData := GetJSON( responseData.DataString );
+    jObject := TJSONObject(jData);
+
+    for pos := 0 to resultVar.Count-1 do
+    begin
+      if jObject.Get(resultVar.Names[pos]) <> Null then
+        resultVar.Values[resultVar.Names[pos]] := jObject.Get(resultVar.Names[pos]);
     end;
-  postVars.Free;
 
-  jData := GetJSON( S );
-  jObject := TJSONObject(jData);
+    resultVar.Values['raw'] := responseData.DataString;
+    Output.Assign(resultVar);
 
-  for pos := 0 to resultVar.Count-1 do
-  begin
-    if jObject.Get(resultVar.Names[pos]) <> Null then
-      resultVar.Values[resultVar.Names[pos]] := jObject.Get(resultVar.Names[pos]);
+  finally
+    responseData.Free;
+    resultVar.Free;
+    postVars.Free;
   end;
-
-  resultVar.Values['raw'] := S;
-  Output := resultVar;
 end;
 
 end.
